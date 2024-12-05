8d86848a90281ee951b723abdafd7e87f8d93df3
lanxuping <lxp120310@163.com>
2024-12-05 09:35:18 +0800

    Initial Commit

---
+++ SwiftUIGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIGame
+//
+//  Created by lanxuping on 2024/12/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIGame/SwiftUIGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGameApp.swift
+//  SwiftUIGame
+//
+//  Created by lanxuping on 2024/12/5.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
